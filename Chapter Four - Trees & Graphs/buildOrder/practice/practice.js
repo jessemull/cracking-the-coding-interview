4413334e7f60bed5b2c08bdd157b15a694478673
Jesse Mull <jessemull@gmail.com>
2024-03-25 21:44:21 -0700

    Removed code from practice

--- Chapter Four - Trees & Graphs/buildOrder/practice/practice.js
+++ Chapter Four - Trees & Graphs/buildOrder/practice/practice.js
@@ -8,31 +8,8 @@ const { chapter, problem } = getChapterAndProblem(__dirname)
 
 // Implement solution here.
 
-const findAndRemoveNoEdges = (graph, built) => {
-  let foundDependencyTOBuild = false
-
-  Object.keys(graph).forEach(key => {
-    const filtered = graph[key].filter(dependency => built.indexOf(dependency) === -1)
-    if (!filtered.length) {
-      foundDependencyTOBuild = true
-      built.push(key)
-      delete graph[key]
-    }
-  })
-
-  return foundDependencyTOBuild
-}
-
 const buildOrder = (graph) => {
-  const built = []
 
-  while (findAndRemoveNoEdges(graph, built)) {}
-
-  if (Object.keys(graph).length === 0) {
-    return built
-  } else {
-    return null
-  }
 }
 
 // Execute all test cases and print the input, output, expected value and pass/fail.
