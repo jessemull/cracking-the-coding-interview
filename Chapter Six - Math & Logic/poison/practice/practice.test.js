15bbacceb841e3a0e503d103ff6aa6ef93bc7e0b
Jesse Mull <jessemull@gmail.com>
2024-04-03 12:23:46 -0700

    Fixed updating poison

--- Chapter Six - Math & Logic/poison/practice/practice.test.js
+++ Chapter Six - Math & Logic/poison/practice/practice.test.js
@@ -1,8 +1,8 @@
 const assert = require('node:assert')
-const longestNoRepeats = require('./practice')
+const poison = require('./practice')
 const test = require('node:test')
 
 test('poison - []', () => {
-  const output = longestNoRepeats()
-  assert.deepEqual(output, 'bbbbb')
+  const output = poison()
+  assert.deepEqual(output, )
 })
