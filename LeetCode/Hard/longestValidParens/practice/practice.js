cbe8ce900ab8d5b3111ac6760c71f794d79cdd8d
Jesse Mull <jessemull@gmail.com>
2024-04-09 08:52:05 -0700

    Remove practice code

--- LeetCode/Hard/longestValidParens/practice/practice.js
+++ LeetCode/Hard/longestValidParens/practice/practice.js
@@ -1,39 +1,7 @@
 // Implement solution here.
 
-const Stack = require('../../../../Data Structures/stack')
-
 const longestValidParens = (s) => {
-  if (s === null || s === undefined || s.length > Math.pow(10, 4) || !/^[\[\]\{\}\(\)]*$/.test(s)) {
-    return null
-  }
 
-  const stack = new Stack()
-
-  for (let i = 0; i < s.length; i++) {
-    let char = s[i]
-    if (char === '(') {
-      stack.push(i)
-    } else {
-      if (!stack.isEmpty() && s[stack.peek()] === '(') {
-        stack.pop()
-      } else {
-        stack.push(i)
-      }
-    }
-  }
-
-  let result = 0
-  let last = s.length
-
-  while(!stack.isEmpty()) {
-    let next = stack.pop()
-    result = Math.max(result, last - next - 1)
-    last = next
-  }
-
-  result = Math.max(result, last)
-
-  return result
 }
 
 module.exports = longestValidParens
